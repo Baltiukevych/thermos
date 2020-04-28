13cb7481d7924fc0831b3520825a205c13be27c8
ili182 <shilliach@gmail.com>
2020-04-28 20:35:50 +0200

    add

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+user = "Janusz"
 
+@app.route("/")
 @app.route("/index")
 def index():
-    return "yo yo dzialam"
+    return render_template("index.html", user=user)
 
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run()

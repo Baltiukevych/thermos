628d5d8f9666935ccdf1f0df8d0c7f3b4f27e597
ili182 <shilliach@gmail.com>
2020-04-29 19:23:59 +0200

    add bookmarks

--- app.py
+++ app.py
@@ -2,28 +2,47 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+bookmark = [
+    {
+        "user": "Janusz",
+        "url": "faceebok.com",
+        "description": "opis"
+    },
+    {
+        "user": "Janusz",
+        "url": "krakow.com",
+        "description": "opis"
+    },
+    {"user": "Rychard",
+     "url": "google.com",
+     "description": "opis"
+     }
+]
 
-user = "Janusz"
+def get_latest_bookmarks(limit):
+    return bookmarks[:limit]
+
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index2")
 def index():
-    return render_template("index.html", user=user)
+    return render_template("index.html", bookmarks=get_latest_bookmarks(2))
 
 
 @app.route("/add")
 def add():
     return render_template("add.html")
 
+
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
-def page_not_found(e):
+def server_error(e):
     return render_template('500.html'), 500
 
+
 if __name__ == "__main__":
-    app.run()
-
-
+    app.run(debug=True)
